bb25eb3272d6ad6508b55d8d4c886638e66a95c6
Dennis Ranke <dennis.ranke@gmail.com>
2016-08-06 13:30:36 +0200

    move example png load/save code to shared file

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,20 +1,19 @@
 extern crate exoquant;
-extern crate lodepng;
 
-use exoquant::{Color, SimpleColorSpace};
+mod png;
+
+use exoquant::*;
 use exoquant::remapper::*;
 
 fn main() {
     println!("Loading PNG");
-    let input = lodepng::decode32_file("test.png").unwrap();
-    let input_image: Vec<_> =
-        input.buffer.as_ref().iter().map(|c| Color::rgba(c.r, c.g, c.b, c.a)).collect();
+    let (input_image, width, height) = png::load("test.png");
 
     let colorspace = SimpleColorSpace::default();
 
     println!("Building histogram");
     let mut hist = exoquant::Histogram::new();
-    hist.extend(input_image.iter().map(|c| *c));
+    hist.extend(input_image.iter().cloned());
 
     println!("Generating palette");
     let mut quantizer = exoquant::Quantizer::new(&hist, &colorspace);
@@ -29,31 +28,12 @@ fn main() {
     println!("Optimize palette (k-means)");
     let palette = exoquant::optimize_palette(&colorspace, palette, &hist, 8);
 
-    let mut state = lodepng::State::new();
-    for color in &palette {
-        unsafe {
-            lodepng::ffi::lodepng_palette_add(&mut state.info_png().color,
-                                              color.r(),
-                                              color.g(),
-                                              color.b(),
-                                              color.a());
-            lodepng::ffi::lodepng_palette_add(&mut state.info_raw(),
-                                              color.r(),
-                                              color.g(),
-                                              color.b(),
-                                              color.a());
-        }
-    }
-    state.info_png().color.bitdepth = 8;
-    state.info_png().color.colortype = lodepng::ColorType::LCT_PALETTE;
-    state.info_raw().bitdepth = 8;
-    state.info_raw().colortype = lodepng::ColorType::LCT_PALETTE;
-
     println!("Remapping image to palette");
     let remapper = Remapper::new(&palette, &colorspace, DithererFloydSteinberg::checkered());
-    let image: Vec<_> = remapper.remap8(&input_image, input.width);
+    let image: Vec<_> = remapper.remap8(&input_image, width);
 
     println!("Saving PNG");
-    state.encode_file("out.png", &image, input.width, input.height).unwrap();
+    png::save("out.png", &palette, &image, width, height);
+
     println!("done!");
 }
---
+++ examples/png.rs
@@ -0,0 +1,34 @@
+extern crate lodepng;
+
+use exoquant::Color;
+
+pub fn load(filename: &str) -> (Vec<Color>, usize, usize) {
+    let img = lodepng::decode32_file(filename).unwrap();
+    (img.buffer.as_ref().iter().map(|c| Color::rgba(c.r, c.g, c.b, c.a)).collect(),
+     img.width,
+     img.height)
+}
+
+pub fn save(filename: &str, palette: &[Color], image: &[u8], width: usize, height: usize) {
+    let mut state = lodepng::State::new();
+    for color in palette {
+        unsafe {
+            lodepng::ffi::lodepng_palette_add(&mut state.info_png().color,
+                                              color.r(),
+                                              color.g(),
+                                              color.b(),
+                                              color.a());
+            lodepng::ffi::lodepng_palette_add(&mut state.info_raw(),
+                                              color.r(),
+                                              color.g(),
+                                              color.b(),
+                                              color.a());
+        }
+    }
+    state.info_png().color.bitdepth = 8;
+    state.info_png().color.colortype = lodepng::ColorType::LCT_PALETTE;
+    state.info_raw().bitdepth = 8;
+    state.info_raw().colortype = lodepng::ColorType::LCT_PALETTE;
+
+    state.encode_file(filename, image, width, height).unwrap();
+}
